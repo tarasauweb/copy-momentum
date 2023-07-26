5f9577ba33383999be17212ab0303d01a1caa65a
Artur Tarasau <tarasov.a.web@gmail.com>
2023-07-26 15:48:32 +0300

    fix:menu bags

--- src/components/menu.ts
+++ src/components/menu.ts
@@ -2,56 +2,75 @@ function Menu () : string {
   const menu:string =  `
     <ul class="menu">
       <li class="menu__items">
-        <span class="menu__span">
-          BackGround
-        </span>
-        <ul class="menu__item">
-          <li class="menu__subitem">
-            flickr
+        <ul class="menu__subscribe">
+          <li class="menu__span">
+            BackGround
           </li>
-          <li class="menu__subitem">
-            github
-          </li>
-          <li class="menu__subitem">
-            unsplash
+        </ul>
+        <ul class="menu__nav">
+          <li class="menu__subitems">
+            <ul class="menu__submenu">
+              <li class="menu__subitem">
+                flickr
+              </li>
+              <li class="menu__subitem">
+                github
+              </li>
+              <li class="menu__subitem">
+                unsplash
+              </li>
+            </ul>
           </li>
         </ul>
       </li>
       <li class="menu__items">
-        <span class="menu__span">
-          Close widget
-        </span>
-        <ul class="menu__item">
-          <li class="menu__subitem">
-            player
+        <ul class="menu__subscribe">
+          <li class="menu__span">
+            Close widget
           </li>
-          <li class="menu__subitem">
-            slider
-          </li>
-          <li class="menu__subitem">
-            time
-          </li>
-          <li class="menu__subitem">
-            date
-          </li>
-          <li class="menu__subitem">
-            greeting
+        </ul>
+        <ul class="menu__nav">
+          <li class="menu__subitems">
+            <ul class="menu__submenu">
+              <li class="menu__subitem">
+                player
+              </li>
+              <li class="menu__subitem">
+                slider
+              </li>
+              <li class="menu__subitem">
+                time
+              </li>
+              <li class="menu__subitem">
+                date
+              </li>
+              <li class="menu__subitem">
+                greeting
+              </li>
+            </ul>
           </li>
         </ul>
       </li>
       <li class="menu__items">
-        <span class="menu__span">
-          Language
-        </span>
-        <ul class="menu__item menu__lang">
-          <li class="menu__subitem">
-            Eng
+        <ul class="menu__subscribe">
+          <li class="menu__span">
+            Language
           </li>
-          <li class="menu__subitem">
-            Bel
-          </li>
-          <li class="menu__subitem">
-            Rus
+        </ul>
+       
+        <ul class="menu__nav">
+          <li class="menu__subitems">
+            <ul class="menu__submenu">
+              <li class="menu__subitem">
+                Eng
+              </li>
+              <li class="menu__subitem">
+                Bel
+              </li>
+              <li class="menu__subitem">
+                Rus
+              </li>
+            </ul>
           </li>
         </ul>
       </li>
