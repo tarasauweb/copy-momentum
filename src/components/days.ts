8f0dd148d9061e7f4ad4394fd2293aeecf0a39c9
Artur Tarasau <tarasov.a.web@gmail.com>
2023-09-24 21:00:34 +0300

    fix:err set day

--- src/components/days.ts
+++ src/components/days.ts
@@ -1,11 +1,11 @@
 enum  EnumDays {
+  "Sunday" = 0,
   'Monday' = 1,
   "Tuesday" = 2,
   "Wednesday" = 3,
   "Thursday" = 4,
   "Friday" = 5,
   "Saturday" = 6,
-  "Sunday" = 7
 }
 
 export default EnumDays
